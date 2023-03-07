2c439d44d20c9375ea4326dcbc738fce031d0b09
mindulle <mindullestudio@gmail.com>
2023-03-07 12:35:26 +0000

    chore: move directory

--- snippets/navi/frequently-used/built-in/install-to-wsl.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:: Install Windows Terminal. After this command is executed, click install button on your monitor.
-Start-Process 'https://aka.ms/terminal'
-
-:: Install windows subsytem for linux automatically.
-wsl.exe --install
